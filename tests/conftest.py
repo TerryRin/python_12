10b853c8974667118e627c33750bc16ac63a34f5
Ekaterina Polushina <terryrinkelly@gmail.com>
2024-05-19 14:58:48 +0300

    Скорректирован conftest и env

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,20 @@ import os
 
 import pytest
 from dotenv import load_dotenv
-from selene import browser
+from selene import Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from utils import attach
+
+DEFAULT_BROWSER_VERSION = "122.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='122.0'
+    )
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -16,27 +25,32 @@ def load_env():
 
 @pytest.fixture(scope='function')
 def browser_manager(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "122.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
-    selenoid_login = os.getenv("SELENOID_LOGIN")
-    selenoid_pass = os.getenv("SELENOID_PASS")
-    # selenoid_url = os.getenv("SELENOID_URL")
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser.config.driver = driver
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+
+    browser = Browser(Config(driver))
+    # browser.config.driver = driver
+    # browser.config.base_url = 'https://demoqa.com'
+    # browser.config.window_width = 1920
+    # browser.config.window_height = 1080
     yield browser
 
     attach.add_screenshot(browser)
